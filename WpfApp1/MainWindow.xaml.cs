17857a5dd25b49954012f1ef9a2a436657b2ef10
User <User@WIN-A9GL8ECUKAH>
2023-09-04 18:29:27 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        Button button1 = new Button() { Content = "Submit", Foreground = new SolidColorBrush(Colors.DarkGreen), HorizontalAlignment=HorizontalAlignment.Center, VerticalAlignment=VerticalAlignment.Center };
+        grid1.Children.Add(button1);
+        }
+        private void BClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
